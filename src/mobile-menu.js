f45e05e1f8d2babb7218ef71e2655a5fafe3725f
Dmytro Yeskov <wavefifth@gmail.com>
2021-12-26 20:01:01 +0200

    close mobile menu upon item selection

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -2,21 +2,19 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const selectMenuItem = document.querySelectorAll('.mobile-container__menu [item]');
+
+  console.log(selectMenuItem);
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  selectMenuItem.forEach(item => item.addEventListener('click', toggleMenu));
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
